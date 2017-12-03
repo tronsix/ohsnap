1153e54370dd88ca582eaaa4991cc109897ae707
Zach Coss <zacharycoss@gmail.com>
2017-12-02 18:56:30 -0600

    updating photos response 2

--- backend/router.js
+++ backend/router.js
@@ -1,8 +1,6 @@
 module.exports = function(app) {
 
-    // Interact with the AWS api.
-    function lookForPhotos(req,res,next) {
-        var async = require('async')
+    function createS3Object() {
         var aws = require('aws-sdk')
         aws.config = new aws.Config()
         aws.config.accessKeyId = process.env.AWS_ACCESS_KEY_ID
@@ -10,14 +8,22 @@ module.exports = function(app) {
         aws.config.region = "us-east-1"
         var s3 = new aws.S3()
         var s3Params = { Bucket: 'nameless-fortress-95164' }
+        return [s3,s3Params]
+    }
+
+    // Adds all images to the homepage by sending an object with each url in the bucket.
+    function showAllPhotos(req,res,next) {
+        var s3array = createS3Object()
+        var s3 = s3array[0]
+        var s3Params = s3array[1]
         var imageArray = []
+
         s3.listObjects(s3Params,function(err,data){
             if (err) {
                 console.log('S3 Error: ')
                 console.log(err)
             } else {
                 for (var key in data.Contents) {
-                    var imageName = data.Contents[key]['Key']
                     var url = 'https://s3.amazonaws.com/nameless-fortress-95164/' + imageName
                     imageArray.push(url)
                 }
@@ -28,9 +34,32 @@ module.exports = function(app) {
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
             res.json({images:imageArray})
-            // res.render('../html/index', {
-            //     images:imageArray
-            // })
+        })
+    }
+
+    // Returns url for the group
+    function lookForGroup(req,res,next,group) {
+        var s3array = createS3Object()
+        var s3 = s3array[0]
+        var s3Params = s3array[1]
+        var imageArray = []
+
+        s3.listObjects(s3Params,function(err,data){
+            if (err) {
+                console.log('S3 Error: ')
+                console.log(err)
+            } else {
+                for (var key in data.Contents) {
+                    var url = 'https://s3.amazonaws.com/nameless-fortress-95164/' + imageName
+                    imageArray.push(url)
+                }
+            }
+            console.log('Looking for photos')
+            res.header("Access-Control-Allow-Origin", '*');
+            res.header("Access-Control-Allow-Credentials", true);
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+            res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+            res.json({images:imageArray})
         })
     }
 
@@ -51,16 +80,16 @@ module.exports = function(app) {
     }
 
     // Set up variables.
+    var async = require('async')
     var express = require('express')
     var apiRouter = express.Router()
     var MessagingResponse = require('twilio').twiml.MessagingResponse
 
     apiRouter.get('*', function(req,res,next) {
         var url = req.originalUrl
-        if (url == '/') {
-            lookForPhotos(req,res,next)
-        } else if (url == '/photos') {
-            lookForPhotos(req,res,next)
+        // Sends object to page with all images.
+        if (url == '/photos') {
+            showAllPhotos(req,res,next)
         } else {
             res.send('Cant find this page: ' + url)
         }
---
+++ squarespace.js
@@ -0,0 +1,47 @@
+<style>
+img{
+    max-width: 100%;
+    height: auto;
+}
+#image-container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.image-div {
+  max-width: 350px;
+}
+</style>
+<script>
+// Make request to server for images
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       // document.getElementById("demo").innerHTML = xhttp.responseText;
+       console.log(xhttp.response)
+       console.log(typeof xhttp.response)
+       var json = JSON.parse(xhttp.response)
+       var images = json.images
+       var div = document.createElement('div')
+       div.id = 'image-container'
+       document.getElementById('container-content').style.padding = 0
+       document.getElementById('container-content').appendChild(div)
+
+       for (var i = 0; i < images.length; i++) {
+          var image = document.createElement('img')
+          image.src = images[i]
+          var imgDiv = document.createElement('div')
+          imgDiv.className = 'image-div'
+          imgDiv.appendChild(image)
+          document.getElementById('image-container').appendChild(imgDiv)
+       }
+    }
+};
+xhttp.open("GET", "https://nameless-fortress-95164.herokuapp.com/photos", true);
+xhttp.send();
+</script>
+
+
