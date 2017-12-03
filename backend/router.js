2d2dfff487f92fb9d4e1fd81cfdec93b1eee9f86
Zach Coss <zacharycoss@gmail.com>
2017-12-02 21:07:49 -0600

    twilio

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,10 @@
 module.exports = function(app) {
 
+    var async = require('async')
+    var express = require('express')
+    var apiRouter = express.Router()
+    var MessagingResponse = require('twilio').twiml.MessagingResponse
+
     function createS3Object() {
         var aws = require('aws-sdk')
         aws.config = new aws.Config()
@@ -10,82 +15,7 @@ module.exports = function(app) {
         var s3Params = { Bucket: 'nameless-fortress-95164' }
         return [s3,s3Params]
     }
-
-    // Adds all images to the homepage by sending an object with each url in the bucket.
-    function showAllPhotos(req,res,next) {
-        var s3array = createS3Object()
-        var s3 = s3array[0]
-        var s3Params = s3array[1]
-        var imageArray = []
-
-        s3.listObjects(s3Params,function(err,data){
-            if (err) {
-                console.log('S3 Error: ')
-                console.log(err)
-            } else {
-                for (var key in data.Contents) {
-                    var imageName = data.Contents[key]['Key']
-                    var url = 'https://s3.amazonaws.com/nameless-fortress-95164/' + imageName
-                    imageArray.push(url)
-                }
-            }
-            console.log('Looking for photos')
-            res.header("Access-Control-Allow-Origin", '*');
-            res.header("Access-Control-Allow-Credentials", true);
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-            res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-            res.json({images:imageArray})
-        })
-    }
-
-    // Returns url for the group
-    function lookForGroup(req,res,next,groupNumber) {
-        var s3array = createS3Object()
-        var s3 = s3array[0]
-        var s3Params = s3array[1]
-        // console.log(s3)
-        // console.log(s3Params)
-        var groupArray = []
-        var twiml = new MessagingResponse()
-
-        s3.listObjects(s3Params,function(err,data){
-            if (err) {
-                console.log('S3 Error: ')
-                console.log(err)
-            } else {
-                if (data.Contents.length < 1) {
-                    twiml.message('Visit the Trail of Lights Facebook page to see your photos!')
-                } else {
-                    for (var key in data.Contents) {
-                        var imageName = data.Contents[key]['Key']
-                        var noExtension = imageName.replace('.jpg','')
-                        var group = noExtension.split('_')[0]
-                        if (Number(group) == Number(groupNumber)) {
-                            var url = 'https://nameless-fortress-95164.herokuapp.com/' + imageName
-                            groupArray.push(url)
-                        }
-                    }
-                    if (groupArray.length < 1) {
-                        twiml.message('Visit the Trail of Lights Facebook page to see your photos!')
-                    } else {
-                        for (i = 0; i < groupArray.length; i++) { 
-                            twiml.message(groupArray[i])
-                        }
-                    }
-                }
-
-                res.header("Access-Control-Allow-Origin", '*');
-                res.header("Access-Control-Allow-Credentials", true);
-                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-                res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     
-                res.writeHead(200, {'Content-Type': 'text/xml'})
-                res.end(twiml.toString())
-
-            }
-        })
-    }
-
     function sendGroupUrl(req,res,next,group) {
         var twiml = new MessagingResponse()
         var url = 'https://nameless-fortress-95164.herokuapp.com/' + group
@@ -99,24 +29,36 @@ module.exports = function(app) {
     }
 
     function getGroupImages(req,res,next,group) {
+        var s3array = createS3Object()
+        var s3 = s3array[0]
+        var s3Params = s3array[1]
+        var twiml = new MessagingResponse()
         var images = []
 
-        res.render('../html/index',{
-            groupNumber:group
+        s3.listObjects(s3Params,function(err,data){
+            if (err) { console.log(err) } 
+            else {
+                for (var key in data.Contents) {
+                    var imageName = data.Contents[key]['Key']
+                    var noExtension = imageName.replace('.jpg','')
+                    var group = noExtension.split('_')[0]
+                    if (Number(group) == Number(groupNumber)) {
+                        var url = 'https://s3.amazonaws.com/nameless-fortress-95164/' + imageName
+                        images.push(url)
+                    }
+                }
+                res.render('../html/index',{
+                    imageArray:images
+                })
+            }
         })
     }
 
-
-    // Set up variables.
-    var async = require('async')
-    var express = require('express')
-    var apiRouter = express.Router()
-    var MessagingResponse = require('twilio').twiml.MessagingResponse
-
     apiRouter.get('*', function(req,res,next) {
         var url = req.originalUrl
+        var group = url.replace('/','')
         if (url == '/photos') {
-            showAllPhotos(req,res,next)
+            // Send all images 10 or 20 at a time
         } else {
             getGroupImages(req,res,next,group)
         }
@@ -135,7 +77,5 @@ module.exports = function(app) {
 
     // Pass all routes to the apiRouter.
     app.use('*',apiRouter)
-
-
 
 }
