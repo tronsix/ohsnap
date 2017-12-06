1f3e4a6bef802f931a65eda0fc21daab7a165905
Zach Coss <zacharycoss@gmail.com>
2017-12-06 00:24:46 -0600

    testing cors 2

--- backend/router.js
+++ backend/router.js
@@ -46,6 +46,7 @@ function getGroupImages(req,res,next,group) {
 function handleAllRequests(req,res,next) {
     // For Cors-Header requests
     // Allows cross-origin requests from any origin host.
+    console.log(req.get('origin'))
     res.header("Access-Control-Allow-Origin", req.headers.host)
     res.header("Access-Control-Allow-Credentials", false)
     res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
